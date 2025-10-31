fd6cb8b288dcdb0d0849ccb59bb11ce397ce92f7
VELYX <vanya.yepez@gmail.com>
2025-10-31 14:56:13 -0600

    Adding zip function

---
+++ functional_programming/zip_function.py
@@ -0,0 +1,16 @@
+valid_formats = [
+    "docx",
+    "pdf",
+    "txt",
+    "pptx",
+    "ppt",
+    "md",
+]
+
+# Don't edit above this line
+
+
+def pair_document_with_format(doc_names, doc_formats):
+    document_list = zip(doc_names, doc_formats)
+    filtered_documents = filter(lambda x: x[1] in valid_formats, document_list)
+    return list(filtered_documents)
